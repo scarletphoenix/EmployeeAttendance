64fff1055533c739f4f5fc65e1eee82180bd075a
Rashmi <agrawal.rashmi9530@gmail.com>
2018-11-09 18:14:51 +0530

    Updates

---
+++ precache-manifest.cc270ac9c694c00c91068a33779c741f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "205365be964d7cde80a2db2b730389e8",
+    "url": "/EmployeeAttendance/static/media/love.205365be.csv"
+  },
+  {
+    "revision": "fdb1ec0060adae38b2c787c1e44c9890",
+    "url": "/EmployeeAttendance/static/media/kay.fdb1ec00.csv"
+  },
+  {
+    "revision": "92202ad0684a6c77a236fabd001c508b",
+    "url": "/EmployeeAttendance/static/media/ho.92202ad0.csv"
+  },
+  {
+    "revision": "545e5c2300271305d532398a64d53ae3",
+    "url": "/EmployeeAttendance/static/media/harry.545e5c23.csv"
+  },
+  {
+    "revision": "3fc367326a4f6a6b472171cc6e7f8bd1",
+    "url": "/EmployeeAttendance/static/media/denny.3fc36732.csv"
+  },
+  {
+    "revision": "a802de5cf8a12d20b849",
+    "url": "/EmployeeAttendance/static/js/runtime~main.a802de5c.js"
+  },
+  {
+    "revision": "aa60a94e58996fd15fe3",
+    "url": "/EmployeeAttendance/static/js/main.aa60a94e.chunk.js"
+  },
+  {
+    "revision": "ab6bdfbbda71c04586bf",
+    "url": "/EmployeeAttendance/static/js/1.ab6bdfbb.chunk.js"
+  },
+  {
+    "revision": "aa60a94e58996fd15fe3",
+    "url": "/EmployeeAttendance/static/css/main.0c600224.chunk.css"
+  },
+  {
+    "revision": "a6dac1d3c1bff245416484e5aa7570e5",
+    "url": "/EmployeeAttendance/index.html"
+  }
+];
